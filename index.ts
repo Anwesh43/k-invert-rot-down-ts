1a4296d41727db2fca7aad0011b8a681608203ac
Anwesh43 <anweshthecool0@gmail.com>
2021-02-02 01:42:17 +0530

    adding KInvertRotDown class

--- index.ts
+++ index.ts
@@ -187,3 +187,26 @@ class IRDNode {
         return this 
     }
 }
+
+class KInvertRotDown {
+
+    curr : IRDNode = new IRDNode(0)
+    dir : number = 1 
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+}
