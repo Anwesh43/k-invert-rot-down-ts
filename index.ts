7ae63d4f57d0a2bafa52f7422a121f3c659e8b28
Anwesh43 <anweshthecool0@gmail.com>
2021-02-02 01:39:23 +0530

    adding IRDNode class

--- index.ts
+++ index.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class IRDNode {
+
+    state : State = new State()
+    prev : IRDNode 
+    next : IRDNode 
+    
+    constructor(private i : number) {
+        this.addNeigbhbor()
+    }
+
+    addNeigbhbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new IRDNode(this.i + 1) 
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawIRDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+    
+    getNext(dir : number, cb : Function) : IRDNode {
+        var curr : IRDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
